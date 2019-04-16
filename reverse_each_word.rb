8b66aa0a0ef6aff569179c2fe15e4f6831427d4a
Erin Stalzer <erinstalzer@gmail.com>
2019-04-16 02:26:42 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,7 @@
+
+
 def reverse_each_word(string)
   arr = string.split(" ")
-  new_arr =[]
-  arr.each do |word|
-    new_arr << word.reverse
-  end
-  new_arr.join(" ")
+  arr.collect {|string| string.reverse}
+  arr.join(" ")
 end
